265e3f9d5ec48092d03727c2ab03cb2c5d80dc8c
SreevaniT <srtonipe@gmail.com>
2020-07-18 20:38:02 +0530

    third commit

---
+++ authProject/urls.py
@@ -0,0 +1,28 @@
+"""authProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from testapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.home_view,name='home_view'),
+    path('java/',views.java_view,name='java_view'),
+    path('python/',views.python_view,name='python_view'),
+    path('aptitude/',views.apti_view,name='apti_view'),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('logout/',views.logout_view,name='logout_view'),
+    path('sign/',views.sign_up_view,name='sign_up_view'),
+]
